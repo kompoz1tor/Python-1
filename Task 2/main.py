28ee5d6ccba87e560d3fecfba26108ce58897a39
Zhavoronkov Sergey <zhavoronkov-s@mail.ru>
2022-12-12 18:27:10 +0300

    Практическое задание №1

--- Task 2/main.py
+++ Task 2/main.py
@@ -1,1 +1,5 @@
 #2* Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+for x in [True,False]:
+    for y in [True,False]:
+        for z in [True,False]:
+            print(x,'AND',y,'OR',z,'=',x and y or z)
